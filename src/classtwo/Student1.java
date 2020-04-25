48df5965afb062bf4c7aacf578d65a5d673259b1
winta123 <meretsa123@gmail.com>
2020-04-25 16:40:58 -0400

    hoho

---
+++ src/classtwo/Student1.java
@@ -0,0 +1,21 @@
+package classtwo;
+class emp{
+	int eid;
+	double salary;
+	String ceo;
+	public void showerthod() {
+		System.out.println(eid +": " + salary +": " + ceo);	}
+}
+
+public class Student1 {
+
+	public static void main(String[] args) {
+		
+			emp obj1;
+			obj1 = new emp();
+			emp obj2 = new emp();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/classtwo/operators.java
@@ -0,0 +1,24 @@
+package classtwo;
+
+public class operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		if (a==5 || b==7) {
+			for (int i=1; i<=3; i++)
+			{
+				for (int j=1; j<5; j++)
+				{
+			System.out.print(j + " ");
+				}
+			System.out.println();
+		}
+	}
+	}
+}
+
+
+	
+
